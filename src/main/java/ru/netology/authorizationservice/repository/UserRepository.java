c51ffd9e996daaebcf8a15d6c2d57c4964dccf9e
Stanislav Rakitov <prosperousrf@gmail.com>
2022-11-15 18:01:14 +0300

    UserRepository

--- src/main/java/ru/netology/authorizationservice/repository/UserRepository.java
+++ src/main/java/ru/netology/authorizationservice/repository/UserRepository.java
@@ -1,14 +1,35 @@
 package ru.netology.authorizationservice.repository;
 
+import ru.netology.authorizationservice.exception.InvalidCredentials;
 import ru.netology.authorizationservice.model.Authorities;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Stanislav Rakitov
  */
 public class UserRepository {
     public List<Authorities> getUserAuthorities(String user, String password) {
-        return ...;
+
+        // Dumb hardcode just for test.
+        List<Authorities> list = new ArrayList<>();
+        Map<String, String> users = new HashMap<>();
+        users.put("alex", "111");
+        users.put("bob", "222"); // Bob never get lucky!
+
+        // Super-duper simple
+        if (users.containsKey(user) && users.get(user).equals(password)) {
+            // Alex got lucky
+            if ("alex".equals(user)) {
+                list.add(Authorities.READ);
+            } // Bob will not have any Authorities
+        } else {
+            throw new InvalidCredentials("Username or password invalid");
+        }
+
+        return list;
     }
 }
